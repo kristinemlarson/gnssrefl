08df013ed9392e18065eda7d1861d290199dacd7
Kristine Larson <kristinem.larson@gmail.com>
2020-09-18 08:55:22 -0600

    more

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy","wget","scipy","matplotlib","requests"]
 
 setup(
     name="gnssrefl",
-    version="0.0.13",
+    version="0.0.14",
     author="Kristine Larson",
     author_email="kristine.larson@colorado.edu",
     description="A GNSS reflectometry software package ",
