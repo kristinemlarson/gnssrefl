b4906a8677c24de688bff5f8577c67786cedd9c5
Kristine Larson <kristinem.larson@gmail.com>
2020-12-01 13:38:08 -0700

    added optional mm,dd,hh,ss

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy","wget","scipy","matplotlib","requests"]
 
 setup(
     name="gnssrefl",
-    version="0.0.36",
+    version="0.0.37",
     author="Kristine Larson",
     author_email="kristine.larson@colorado.edu",
     description="A GNSS reflectometry software package ",
