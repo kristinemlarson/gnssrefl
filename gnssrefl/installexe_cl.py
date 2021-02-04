2f9ad8d5d6e1594c52801982d2e648b8b43cbad7
kristinemlarson <kristinem.larson@gmail.com>
2021-02-04 14:07:36 -0700

    octal permissions

--- gnssrefl/installexe_cl.py
+++ gnssrefl/installexe_cl.py
@@ -3,8 +3,9 @@
 installs non-python executables
 """
 import argparse
-import gnssrefl.gps as g
+import wget
 import os
+import subprocess
 import sys
 
 
@@ -13,25 +14,51 @@ def main():
     command line interface to install non-python executables, specifically
     CRX2RNX, gpsSNR.e, and gnssSNR.e  
     I will add gfzrnx when I have a chance.
-
+    https://stackoverflow.com/questions/12791997/how-do-you-do-a-simple-chmod-x-from-within-python
     author: kristine larson
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("os", help="operating system (linux or macos)", type=str)
+    parser.add_argument("opsys", help="operating system (linux or macos)", type=str)
 # optional arguments
 
     args = parser.parse_args()
 
-    opsys = args.os
+    opsys = args.opsys
     exedir = os.environ['EXE']
-    print(exedir)
+    print('executable environment area:', exedir)
 
-    if opsys == 'linux':
-        print('linux')
+    # download executable and mv to EXE area
+    if (opsys == 'linux'):
+        savename = 'CRX2RNX'
+        url = 'https://github.com/kristinemlarson/gnssrefl/blob/master/static/CRX2RNX.linux64.e'
+        wget.download(url,savename)
+        os.chmod('CRX2RNX',0o111)
+        subprocess.call('mv', savename, exedir)
+        savename = 'gpsSNR.e'
+        url = 'https://github.com/kristinemlarson/gnssrefl/blob/master/static/gpsSNR.linux64.e'
+        wget.download(url,savename)
+        os.chmod(savename,0o111)
+        subprocess.call('mv', savename, exedir)
+
+    elif (opsys == 'macos'):
+        savename = 'CRX2RNX'
+        url = 'https://github.com/kristinemlarson/gnssrefl/blob/master/static/CRX2RNX.macos.e'
+        print(url,savename,exedir)
+        wget.download(url,savename)
+        os.chmod(savename,0o111)
+        subprocess.call('mv', savename, exedir)
+
+        savename = 'gpsSNR.e'
+        url = 'https://github.com/kristinemlarson/gnssrefl/blob/master/static/gpsSNR.macos.e'
+        print(url,savename,exedir)
+        wget.download(url,savename)
+        os.chmod(savename,0o111)
+        subprocess.call('mv', savename, exedir)
 
     else:
-        print('macos')
+        print('do not recognize your input. Exiting.')
+        sys.exit()
 
 
 if __name__ == "__main__":
