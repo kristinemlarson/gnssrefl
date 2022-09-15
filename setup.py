fd0ea83a999882142d1859d8dfbdbd8440b2ed69
kristinemlarson <kristinem.larson@gmail.com>
2022-09-15 13:30:21 -0600

    make new version to upload to pypi, fixed bugs in invsnr snr upload

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = ["numpy","wget","scipy","matplotlib","requests","progress","astro
 
 setup(
     name="gnssrefl",
-    version="1.1.6",
+    version="1.1.7",
     author="Kristine Larson",
     author_email="kristinem.larson@gmail.com",
     description="A GNSS reflectometry software package ",
