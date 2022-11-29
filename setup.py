0cba9e0e171a78195b440646da68d96ff74beebe
kristinemlarson <kristinem.larson@gmail.com>
2022-11-29 14:58:19 +0100

    new version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = ["numpy","wget","scipy","matplotlib","requests","progress","astro
 
 setup(
     name="gnssrefl",
-    version="1.1.12",
+    version="1.1.13",
     author="Kristine Larson",
     author_email="kristinem.larson@gmail.com",
     description="A GNSS reflectometry software package ",
