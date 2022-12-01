7a1faca982c41e94dbb06a2d8c52d01dd0340792
kristinemlarson <kristinem.larson@gmail.com>
2022-12-01 09:36:27 +0100

    added units to output in subdaily

--- gnssrefl/subdaily.py
+++ gnssrefl/subdaily.py
@@ -594,17 +594,21 @@ def write_out_header(fout,station,extraline,**kwargs):
     fout.write('% gnssrefl, https://github.com/kristinemlarson \n')
     if len(extraline) > 0:
         fout.write('% IMPORTANT {0:s} \n'.format(  extraline ))
-    fout.write('% Phase Center corrections have NOT been applied \n')
+    fout.write('% Tradiational phase center corrections have NOT been applied \n')
     if extra_columns:
-        fout.write("% (1)  (2)   (3) (4)  (5)     (6)   (7)    (8)    (9)   (10)  (11) (12) (13)    (14)     (15)    (16) (17) (18,19,20,21,22) (23)    (24)\n")
+        fout.write("% (1)  (2)   (3) (4)  (5)     (6)   (7)    (8)    (9)   (10)  (11) (12) (13)    (14)     (15)    (16) (17) (18,19,20,21,22) (23)    (24) \n")
         fout.write("% year, doy, RH, sat,UTCtime, Azim, Amp,  eminO, emaxO,NumbOf,freq,rise,EdotF, PkNoise  DelT     MJD  refr  MM DD HH MM SS  newRH  RHcorr\n")
+        fout.write("%             m       hours   deg   v/v    deg   deg                1/-1       ratio    minute        1/0                    m      m    \n")
     else:
         if IFcol:
+            fout.write('% Reflector heights set to the L1-phase center are in column 25 \n')
             fout.write("% (1)  (2)   (3) (4)  (5)     (6)   (7)    (8)    (9)   (10)  (11) (12) (13)    (14)     (15)    (16) (17) (18,19,20,21,22) (23)    (24)   (25)\n")
-            fout.write("% year, doy, RH, sat,UTCtime, Azim, Amp,  eminO, emaxO,NumbOf,freq,rise,EdotF, PkNoise  DelT     MJD  refr  MM DD HH MM SS  newRH  RHcorr  IFcorr\n")
+            fout.write("% year, doy, RH, sat,UTCtime, Azim, Amp,  eminO, emaxO,NumbOf,freq,rise,EdotF, PkNoise  DelT     MJD  refr  MM DD HH MM SS  newRH  RHcorr  IFcorrRH\n")
+            fout.write("%             m       hours   deg   v/v    deg   deg                1/-1       ratio    minute        1/0                    m      m         m  \n")
         else:
             fout.write("% (1)  (2)   (3) (4)  (5)     (6)   (7)    (8)    (9)   (10)  (11) (12) (13)    (14)     (15)    (16) (17) (18,19,20,21,22)\n")
             fout.write("% year, doy, RH, sat,UTCtime, Azim, Amp,  eminO, emaxO,NumbOf,freq,rise,EdotF, PkNoise  DelT     MJD  refr  MM DD HH MM SS \n")
+            fout.write("%             m       hours   deg   v/v    deg   deg                1/-1       ratio    minute        1/0                  \n")
 
 
 def writejsonfile(ntv,station, outfile):
