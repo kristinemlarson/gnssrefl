ac24c791215d315ef5f6e65f8e9d7aea033f6809
kristinemlarson <kristinem.larson@gmail.com>
2023-01-21 12:07:45 -0700

    pushed new changes to quickplt & released new version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = ["numpy","wget","scipy","matplotlib","requests","progress","astro
 
 setup(
     name="gnssrefl",
-    version="1.2.11",
+    version="1.2.12",
     author="Kristine Larson",
     author_email="kristinem.larson@gmail.com",
     description="A GNSS reflectometry software package ",
