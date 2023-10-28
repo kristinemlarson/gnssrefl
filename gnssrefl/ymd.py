874aa6c021cb60f0b4f97d3c74711a2d05755292
kristinemlarson <kristinem.larson@gmail.com>
2023-10-28 16:34:00 +0200

    trying out importlib without putting it in setup.py

--- gnssrefl/ymd.py
+++ gnssrefl/ymd.py
@@ -7,7 +7,7 @@ import argparse
 import gnssrefl.gps as g
 
 # this requires python 3.8
-#from importlib.metadata import version
+from importlib.metadata import version
 
 def main():
     """
@@ -40,7 +40,7 @@ def main():
     # testing out version thing
     # https://stackoverflow.com/questions/3524168/how-do-i-get-a-python-modules-version-number-through-code
     # am not able to install this currenlty in the docker
-    #print('Version number ', version('gnssrefl'))
+    print('Version number ', version('gnssrefl'))
 
     doy,cdoy,cyyyy,cyy = g.ymd2doy(year, month, day )
     print(cdoy)
