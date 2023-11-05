621cabb1174eded2aeb75a9dd1c950a98685526a
kristinemlarson <kristinem.larson@gmail.com>
2023-11-05 11:22:56 +0100

    version 1.9.2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ requirements = [
 ]
 setup(
     name="gnssrefl",
-    version="1.9.1",
+    version="1.9.2",
     author="Kristine Larson",
     author_email="kristinem.larson@gmail.com",
     description="A GNSS reflectometry software package ",
