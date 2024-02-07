d60cec2a16135c56ecfda25888dec85b9a8b473f
kristinemlarson <kristinem.larson@gmail.com>
2024-02-07 09:13:51 +0100

    changing tag after failed docker build

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ requirements = [
 ]
 setup(
     name="gnssrefl",
-    version="2.1",
+    version="2.2.0",
     author="Kristine Larson",
     author_email="kristinem.larson@gmail.com",
     description="A GNSS reflectometry software package ",
