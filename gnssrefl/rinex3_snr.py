994e25b7eee8fdb128cbd44bdf0a2a5ed63ee536
kristinemlarson <kristinem.larson@gmail.com>
2024-08-16 08:57:34 -0700

    added error checking to rinex3_snr

--- gnssrefl/rinex3_snr.py
+++ gnssrefl/rinex3_snr.py
@@ -14,6 +14,7 @@ def main():
 
     Creates SNR file from RINEX 3 file that is stored locally
     Requires the gfzrnx executable to be available.
+    Does not overwrite existing SNR files
 
     Parameters
     ----------
@@ -29,27 +30,36 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("rinex3", help="rinex3 filename", type=str)
     parser.add_argument("-orb", help="orbit choice", default='gbm',type=str)
-    parser.add_argument("-dec", help="decimation", default=None,type=int)
-    parser.add_argument("-snr", help="SNR file ending (default is 66)", default=None,type=str)
-    parser.add_argument("-earthscope_fail", help="boolean(T or True), earthscope created illegal filenames", default=None,type=str)
+    parser.add_argument("-dec", help="decimation, in seconds", default=None,type=int)
+    parser.add_argument("-snr", help="SNR file ending (default is 66)", default=None,type=int)
+    parser.add_argument("-name_fail", help="boolean(T or True), RINEX 3 files masquerading as RINEX 2.11 files", default=None,type=str)
 
 
     args = parser.parse_args()
     if args.snr is None:
-        snr = '66'
+        isnr = 66
     else:
-        snr = int(args.snr)
+        isnr = args.snr
 
     if args.dec is None:
         dec_rate = 0; 
     else:
         dec_rate = args.dec
 
-    if (args.earthscope_fail == 'T') or (args.earthscope_fail == 'True'):
-        # earthscope felt the need to distribute illegal RINEX files.
+    r3=args.rinex3
+    station4ch = r3[0:4].lower()
+    year = int(r3[12:16])
+    doy = int(r3[16:19] )
+    snre = g.snr_exist(station4ch,year,doy,str(isnr))
+
+    if snre : 
+        print('Requested SNR file already exists. Exiting.')
+        sys.exit()
+
+    if (args.name_fail == 'T') or (args.name_fail == 'True'):
         # this is an attempt to help those people impacted by this activity.
         illegal_file = args.rinex3
-        print('Earthscope using an illegal filename for RINEX 3 data:', illegal_file)
+        print('Someone is using a RINEX 2.11 filename for RINEX 3 data:', illegal_file)
 
         station = illegal_file[0:4].lower()
         STATION = illegal_file[0:4].upper() + '00ANT'
@@ -100,13 +110,14 @@ def main():
         print('ERROR: your input file does not exist:', rinex3)
         sys.exit()
     if os.path.isfile(rinex2):
-        print('found version 2 rinex')
-        isnr = 66;  rate = 30; idoy = int(cdoy); iyear = int(year)
-        rate = '30'; 
+        print('found version 2 rinex',rinex2)
+        idoy = int(cdoy); iyear = int(year)
+
+        # many of these are fake values because the file has already been translated to rinex2
         archive = 'unavco' ; fortran = False; translator = 'hybrid'
         year_list = [iyear]; doy_list = [idoy]; rate = 'low';   nol = True; 
         overwrite = False; srate = 30; mk = False; skipit = 1
-        strip = False; stream = 'S'  ; bkg = 'IGS' # many of these are fake values because the file has already been translated to rinex2
+        strip = False; stream = 'R'  ; bkg = 'IGS' 
         gzip = True
         timeout = 0
         screenstats = False
