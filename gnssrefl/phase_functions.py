fix: fix off-by-one error when checking minvalperday in vwc

gnssrefl/phase_functions.py
@@ -812,7 +812,7 @@ def write_avg_phase(station, phase, fr,year,year_end,minvalperday,vxyz,subdir):
             # put in amplitude criteria to keep out bad L2P results
                 ph1 = phase[(y1 == requested_year) & (d1 == doy) & (phase > -10) & (amp > 0.65)]
                 amp1 = amp[(y1 == requested_year) & (d1 == doy) & (phase > -10) & (amp > 0.65)]
-                if (len(ph1) > minvalperday):
+                if (len(ph1) >= minvalperday):
                     newl = [requested_year, doy, np.mean(ph1), len(ph1)]
                 # i think you normalize the individual satellites before this step
                 #namp = qp.normAmp(amp1,0.15)